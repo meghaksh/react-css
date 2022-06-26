3f65386504acc387712cb016de22d7d917fe022a
meghaksh4footprints@gmail.com <meghaksh4footprints@gmail.com>
2022-06-26 13:48:23 -0400

    normal css, styled-components package, css modules

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,8 +1,56 @@
-import { toBeInvalid } from "@testing-library/jest-dom/dist/matchers";
 import React, { useState } from "react";
 
+/**Below is for styled components. Scoped.   */
+import styled from "styled-components";
 import Button from "../../UI/Button/Button";
+/**Below is for css module. Scoped. */
+import styles from "./CourseInput.module.css";
+
+/**Below is for normal css which is not scoped.  */
 import "./CourseInput.css";
+
+/**Below styled-component is replaced with css modules.  */
+/*
+const FormControl = styled.div`
+  margin: 0.5rem 0;
+
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+    color: ${(props) => (props.invalid ? "red" : "black")};
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    // border: 1px solid #ccc;    //this is a comment. styled component comments does not change color somehow.
+    border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
+    background: ${(props) => (props.invalid ? "#ffd7d7" : "transparent")};
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+
+  /*alternate css rules for the inline styles applied in CourseInput.js
+  Commented because we used dynamic style using props in the input and label. 
+  Based on the invalid input. 
+  
+  // &.invalid input {
+  //   border-color: red;
+  //   background: #ffd7d7;
+  // }
+
+  // &.invalid label {
+  //   color: red;
+  // }
+`;*/
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
@@ -44,10 +92,35 @@ const CourseInput = (props) => {
       We have added rules in the css file and now 
       we will assign those rules dynamically
       int the parent.  */}
+      {/* 
+      Replaced with the styled component FormControl. 
       <div className={`form-control ${!isValid ? "invalid" : ""}`}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
+      </div> */}
+
+      {/* 
+      Without using props applying dynamic style. 
+      <FormControl className={!isValid && "invalid"}>
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
+      </FormControl> */}
+
+      {/* Using props to apply dynamic styling.  
+      It is commented as we are using css module below. 
+      */}
+      {/* <FormControl invalid={!isValid}>
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
+      </FormControl> */}
+
+      <div
+        className={`${styles["form-control"]} ${!isValid && styles.invalid}`}
+      >
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
       </div>
+
       <Button type="submit">Add Goal</Button>
     </form>
   );
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,13 +1,68 @@
-import React from 'react';
+import React from "react";
 
-import './Button.css';
+//Below import is for styled components.
+//import styled from "styled-components";
 
-const Button = props => {
+//Below import is for normal css used in react component.
+//import "./Button.css";
+
+//Below import used for css module.
+import styles from "./Button.module.css";
+
+const Button = (props) => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
       {props.children}
     </button>
   );
 };
+
+/**
+ *
+ * Creating styled component.
+ * styled.button will return a button component with styles attached.
+ * For normal css style directly on button, do not use any class name or id.
+ * For psuedo classes, use &:focus, &:hover etc.
+ *
+ * Commenting styled component button as we are using CSS modules.
+ */
+/*
+const Button = styled.button`
+  width: 100%;
+  font: inherit;
+  padding: 0.5rem 1.5rem;
+  border: 1px solid #8b005d;
+  color: white;
+  background: #8b005d;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+  cursor: pointer;
+
+  @media (min-width: 768px) {
+    width: auto;
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  &:hover,
+  &:active {
+    background: #ac0e77;
+    border-color: #ac0e77;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+  }
+`;
+*/
+
+/**Using styled-component package above instead of this older
+ * implementation of custom button.
+ */
+// const Button = props => {
+//   return (
+//     <button type={props.type} className="button" onClick={props.onClick}>
+//       {props.children}
+//     </button>
+//   );
+// };
 
 export default Button;
