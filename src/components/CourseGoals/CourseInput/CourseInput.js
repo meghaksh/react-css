8617b3ab1530d54d7b89e1a66ca8939b4db940b4
meghaksh4footprints@gmail.com <meghaksh4footprints@gmail.com>
2022-06-26 02:32:28 -0400

    pushing changes for dynamically creating style for react

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,3 +1,4 @@
+import { toBeInvalid } from "@testing-library/jest-dom/dist/matchers";
 import React, { useState } from "react";
 
 import Button from "../../UI/Button/Button";
@@ -5,19 +6,45 @@ import "./CourseInput.css";
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
-
+  const [isValid, setIsValid] = useState(true);
   const goalInputChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
+      {/* Applying inline style in below code. 
+        Inline style are not good as they override the existing styles.  */}
+      {/*
       <div className="form-control">
+        
+         <label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label>
+        <input
+          style={{
+            borderColor: !isValid ? "red" : "#ccc",
+            background: !isValid ? "salmon" : "transparent",
+          }}
+          type="text"
+          onChange={goalInputChangeHandler}
+        /> */}
+
+      {/* Alternative approach of inline css. 
+      We have added rules in the css file and now 
+      we will assign those rules dynamically
+      int the parent.  */}
+      <div className={`form-control ${!isValid ? "invalid" : ""}`}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
